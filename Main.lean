e44c83d289a4ddfdd3ae2cda46ceabe94a867e75
Marcus Rossel <marcusrossel@icloud.com>
2022-10-07 14:43:21 +0200

    DSL: Implement InjectiveCoe generation in gen_graph

--- Main.lean
+++ Main.lean
@@ -1,148 +1,63 @@
 import Runtime
 
-/-
-network {
-  main reactor Main {
-    connections := [x.o1 → y.i3]
-
-    reaction first (startup, i1, !i2, !@a1, x.o1) → (o1, x.i2) { 
-      let i ← getInput i1
-      let i' := (i.getD 0) + 1
-      let b := if i' = 0 then true else false
-      setOutput o1 b
-      setOutput o1 true
-      setState s1 (-1 : Int)
-      match ← getState s1 with
-      | none => return
-      | some v => 
-        setState s1 (v * 12)
-        setState s2 false
-    }
-
-    reaction second (i1, !@a1, !@a2) → (o1, @a2) { 
-      let _ := (← getAction a1).map (· ++ "suffix")
-      let _ ← getInput i1
-      schedule a2 112 "First"
-      schedule a2 113 "Second"
-      IO.println "Hello"
-      let dir := IO.appDir
-      let dir' ← IO.appDir
-    }
-  }
-}
--/
-
 gen_graph graph where
-  Main where
-    inputs  := [i1 : Nat, i2 : String]
-    outputs := [o1 : Bool, o2 : Unit]
-    actions := [a1 : String, a2 : Bool × Nat]
-    state   := [s1 : Int, s2 : Bool]
-    nested  := [x : Sub, y : Sub]
-  Sub where
-    inputs  := [i1 : Nat, i2 : Bool, i3 : Unit]
-    outputs := [o1 : Unit]
-    actions := []
-    state   := []
-    nested  := [a : Grand, b : Grand]
-  Grand where
-    inputs  := []
-    outputs := []
-    actions := []
-    state   := []
-    nested  := []
-
-inductive graph.Main.Reaction1.PortSource   | i1 | i2 | x.o1 deriving DecidableEq
-inductive graph.Main.Reaction1.PortEffect   | o1 | x.i2 deriving DecidableEq
-inductive graph.Main.Reaction1.ActionSource | a1 deriving DecidableEq
-inductive graph.Main.Reaction1.ActionEffect deriving DecidableEq
-
-inductive graph.Main.Reaction2.PortSource   | i1 deriving DecidableEq
-inductive graph.Main.Reaction2.PortEffect   | o1 | o2 deriving DecidableEq
-inductive graph.Main.Reaction2.ActionSource | a1 | a2 deriving DecidableEq
-inductive graph.Main.Reaction2.ActionEffect | a2 deriving DecidableEq
-
-@[reducible]
-instance : InjectiveCoe graph.Main.Reaction1.PortSource (graph.reactionInputScheme .Main).vars where
-  coe
-    | .i1   => .inl .i1
-    | .i2   => .inl .i2
-    | .x.o1 => .inr ⟨.x, .o1⟩
-  inv
-    | .inl .i1       => some .i1  
-    | .inl .i2       => some .i2  
-    | .inr ⟨.x, .o1⟩ => some .x.o1
-    | _              => none
-
-@[reducible]
-instance : InjectiveCoe graph.Main.Reaction1.PortEffect (graph.reactionOutputScheme .Main).vars where
-  coe
-    | .o1   => .inl .o1
-    | .x.i2 => .inr ⟨.x, .i2⟩
-  inv 
-    | .inl .o1       => some .o1
-    | .inr ⟨.x, .i2⟩ => some .x.i2
-    | _              => none
-
-@[reducible]
-instance : InjectiveCoe graph.Main.Reaction1.ActionSource (graph.schemes .Main |>.interface .actions |>.vars) where
-  coe
-    | .a1 => .a1
-  inv
-    | .a1 => some .a1
-    | _   => none
-
-@[reducible]
-instance : InjectiveCoe graph.Main.Reaction1.ActionEffect (graph.schemes .Main |>.interface .actions |>.vars) where
-  coe := (·.casesOn)
-  inv _ := none
-
-@[reducible]
-instance : InjectiveCoe graph.Main.Reaction2.PortSource (graph.reactionInputScheme .Main).vars where
-  coe
-    | .i1 => .inl .i1
-  inv
-    | .inl .i1 => some .i1
-    | _        => none
-
-@[reducible]
-instance : InjectiveCoe graph.Main.Reaction2.PortEffect (graph.reactionOutputScheme .Main).vars where
-  coe
-    | .o1 => .inl .o1
-    | .o2 => .inl .o2
-  inv
-    | .inl .o1 => some .o1
-    | .inl .o2 => some .o2
-    | _        => none
-
-@[reducible]
-instance : InjectiveCoe graph.Main.Reaction2.ActionSource (graph.schemes .Main |>.interface .actions |>.vars) where
-  coe
-    | .a1 => .a1
-    | .a2 => .a2
-  inv
-    | .a1 => some .a1
-    | .a2 => some .a2
-
-@[reducible]
-instance : InjectiveCoe graph.Main.Reaction2.ActionEffect (graph.schemes .Main |>.interface .actions |>.vars) where
-  coe
-    | .a2 => .a2
-  inv
-    | .a2 => some .a2
-    | _   => none
-
+  reactor Main
+    inputs    [i1 : Nat, i2 : String]
+    outputs   [o1 : Bool, o2 : Unit]
+    actions   [a1 : String, a2 : Bool × Nat]
+    state     [s1 : Int, s2 : Bool]
+    nested    [x : Sub, y : Sub]
+    reactions [
+      {
+        portSources   [i1, i2, x.o1]
+        portEffects   [o1, x.i2]
+        actionSources [a1]
+        actionEffects []
+        triggers {
+          ports   [i1]
+          actions [a2]
+          meta    [startup, shutdown]
+        }
+        body := sorry
+      }
+      {
+        portSources   [i1]
+        portEffects   [o1, o2]
+        actionSources [a1, a2]
+        actionEffects [a2]
+        triggers {
+          ports   []
+          actions [a2]
+          meta    []
+        }
+        body := sorry
+      }
+    ]
+  reactor Sub
+    inputs    [i1 : Nat, i2 : Bool, i3 : Unit]
+    outputs   [o1 : Unit]
+    actions   []
+    state     []
+    nested    [a : Grand, b : Grand]
+    reactions []
+  reactor Grand
+    inputs    []
+    outputs   []
+    actions   []
+    state     []
+    nested    []
+    reactions []
 abbrev network : Network where
   toGraph := graph
-  reactions 
+  «reactions» 
     | .Main => #[
       {
-        portSources   := graph.Main.Reaction1.PortSource
-        portEffects   := graph.Main.Reaction1.PortEffect
-        actionSources := graph.Main.Reaction1.ActionSource
-        actionEffects := graph.Main.Reaction1.ActionEffect
-        triggers      := #[.startup, .shutdown, .port .i2, .action .a1]
-        body := open ReactionM graph Main Reaction1 PortSource PortEffect ActionSource ActionEffect State in do
+        «portSources»   := graph.Main.Reaction0.PortSource
+        «portEffects»   := graph.Main.Reaction0.PortEffect
+        «actionSources» := graph.Main.Reaction0.ActionSource
+        «actionEffects» := graph.Main.Reaction0.ActionEffect
+        «triggers»      := #[.startup, .shutdown, .port .i2, .action .a1]
+        «body» := open ReactionM graph Main Reaction0 PortSource PortEffect ActionSource ActionEffect State in do
           let i ← getInput i1
           let i' := (i.getD 0) + 1 
           let b := if i' = 0 then true else false
@@ -161,12 +76,12 @@ abbrev network : Network where
           monadLift <| IO.println s!"{← getAction a1}"
       },
       {
-        portSources   := graph.Main.Reaction2.PortSource
-        portEffects   := graph.Main.Reaction2.PortEffect
-        actionSources := graph.Main.Reaction2.ActionSource
-        actionEffects := graph.Main.Reaction2.ActionEffect
-        triggers      := #[.action .a1, .action .a2]
-        body := open ReactionM graph Main Reaction2 PortSource PortEffect ActionSource ActionEffect State in do
+        «portSources»   := graph.Main.Reaction1.PortSource
+        «portEffects»   := graph.Main.Reaction1.PortEffect
+        «actionSources» := graph.Main.Reaction1.ActionSource
+        «actionEffects» := graph.Main.Reaction1.ActionEffect
+        «triggers»      := #[.action .a1, .action .a2]
+        «body» := open ReactionM graph Main Reaction1 PortSource PortEffect ActionSource ActionEffect State in do
           let a ← getAction a1
           -- let _ := a.map (· ++ "suffix") -- TODO: Why doesn't this work?
           let _ ← getInput i1
--- Runtime/DSL/GenGraph.lean
+++ Runtime/DSL/GenGraph.lean
@@ -7,13 +7,29 @@ structure InterfaceVar where
   value : Term
 
 def InterfaceDecl := Array InterfaceVar
-  
-deriving instance Inhabited for InterfaceDecl
+  deriving Inhabited
 
+structure TriggerDecl where
+  ports : Array Ident
+  actions : Array Ident
+  meta : Array Ident
+
+inductive ReactionDecl.DependencyKind
+  | portSource
+  | portEffect
+  | actionSource
+  | actionEffect
+
+structure ReactionDecl where
+  dependencies : ReactionDecl.DependencyKind → Array Ident
+  triggers : TriggerDecl
+  body : TSyntax `Lean.Parser.Term.doSeq
+  
 structure ReactorDecl where
   name : Ident
   interfaces : Reactor.InterfaceKind → InterfaceDecl
   nested : InterfaceDecl
+  reactions : Array ReactionDecl
   deriving Inhabited
 
 structure GraphDecl where
@@ -26,13 +42,34 @@ syntax ident " : " term : interface_var
 declare_syntax_cat interface_decl
 syntax "[" interface_var,* "]" : interface_decl
 
+declare_syntax_cat ident_list
+syntax "[" ident,* "]" : ident_list
+
+declare_syntax_cat trigger_decl
+syntax "triggers" "{"
+  "ports" ident_list
+  "actions" ident_list
+  "meta" ident_list
+  "}" : trigger_decl
+
+declare_syntax_cat reaction_decl
+syntax "{"  
+  "portSources"   ident_list
+  "portEffects"   ident_list
+  "actionSources" ident_list
+  "actionEffects" ident_list
+  trigger_decl
+  "body" ":=" doSeq
+  "}" : reaction_decl
+
 declare_syntax_cat reactor_decl
-syntax ident "where" 
-  "inputs"  ":=" interface_decl 
-  "outputs" ":=" interface_decl 
-  "actions" ":=" interface_decl 
-  "state"   ":=" interface_decl 
-  "nested"  ":=" interface_decl
+syntax "reactor" ident 
+  "inputs"  interface_decl 
+  "outputs" interface_decl 
+  "actions" interface_decl 
+  "state"   interface_decl 
+  "nested"  interface_decl
+  "reactions" "[" reaction_decl* "]"
   : reactor_decl
 
 declare_syntax_cat graph_decl
@@ -46,17 +83,35 @@ def InterfaceDecl.fromSyntax : TSyntax `interface_decl → MacroM InterfaceDecl
   | `(interface_decl| [$vars:interface_var,*]) => vars.getElems.mapM InterfaceVar.fromSyntax
   | _ => throwUnsupported
 
+def TriggerDecl.fromSyntax : TSyntax `trigger_decl → MacroM TriggerDecl
+  | `(trigger_decl| triggers { ports [$p:ident,*] actions [$a:ident,*] meta [$m:ident,*] }) =>
+    return { «ports» := p, «actions» := a, «meta» := m }
+  | _ => throwUnsupported
+
+def ReactionDecl.fromSyntax : TSyntax `reaction_decl → MacroM ReactionDecl 
+  | `(reaction_decl| { 
+      portSources [$ps:ident,*] portEffects [$pe:ident,*] actionSources [$as:ident,*] 
+      actionEffects [$ae:ident,*] $ts:trigger_decl body := $b:doSeq
+    }) => return { 
+      dependencies := fun | .portSource => ps | .portEffect => pe | .actionSource => as | .actionEffect => ae
+      «triggers» := ← TriggerDecl.fromSyntax ts
+      «body» := b
+    }
+  | _ => throwUnsupported
+
 def ReactorDecl.fromSyntax : TSyntax `reactor_decl → MacroM ReactorDecl
-  | `(reactor_decl| $name:ident where inputs := $i outputs := $o actions := $a state := $s nested := $n) => do
+  | `(reactor_decl| reactor $name:ident inputs $i outputs $o actions $a state $s nested $n reactions [$r:reaction_decl*]) => do
     let i ← InterfaceDecl.fromSyntax i
     let o ← InterfaceDecl.fromSyntax o
     let a ← InterfaceDecl.fromSyntax a
     let s ← InterfaceDecl.fromSyntax s
     let n ← InterfaceDecl.fromSyntax n
+    let r ← r.mapM ReactionDecl.fromSyntax
     return {
       name := name
       interfaces := fun | .inputs => i | .outputs => o | .actions => a | .state => s
-      «nested» := n  
+      «nested» := n
+      «reactions» := r  
     }
   | _ => throwUnsupported
 
@@ -73,12 +128,44 @@ def InterfaceDecl.ids (decl : InterfaceDecl) :=
 def InterfaceDecl.values (decl : InterfaceDecl) := 
   decl.map (·.value)
 
+def ReactorDecl.num (decl : ReactorDecl) (kind : Reactor.InterfaceKind) :=
+  decl.interfaces kind |>.size
+
 def GraphDecl.reactorNames (decl : GraphDecl) :=
   decl.reactors.map (·.name)
 
 def GraphDecl.mainReactorIdent! (decl : GraphDecl) :=
   let reactorIdent := decl.reactors[0]!.name
   mkIdentFrom reactorIdent (decl.name.getId ++ `Class ++ reactorIdent.getId)
+
+def GraphDecl.reactorWithName (decl : GraphDecl) (className : Name) : MacroM ReactorDecl :=
+  match decl.reactors.find? (·.name.getId = className) with
+  | some rtr => return rtr
+  | none => Macro.throwError s!"GraphDecl.reactorWithName: Unknown reactor '{className}'"
+
+def GraphDecl.numNested (decl : GraphDecl) (rtr : Name) (kind : Reactor.InterfaceKind) : MacroM Nat := do
+  let rtr ← decl.reactorWithName rtr
+  rtr.nested.values.foldlM (init := 0) fun acc «class» => do
+    match «class» with 
+    | `($c:ident) => 
+      let nestedReactor ← decl.reactorWithName c.getId
+      return acc + nestedReactor.num kind
+    | _ => Macro.throwError s!"GraphDecl.numNested: Illformed reactor class '{«class»}'"
+
+def GraphDecl.numSources (decl : GraphDecl) (rtr : Name) : MacroM Nat := do
+  let numLocalInputs := (← decl.reactorWithName rtr).num .inputs
+  let numNestedOutputs ← decl.numNested rtr .outputs
+  return numLocalInputs + numNestedOutputs
+
+def GraphDecl.numEffects (decl : GraphDecl) (rtr : Name) : MacroM Nat := do
+  let numLocalOutputs := (← decl.reactorWithName rtr).num .outputs
+  let numNestedInputs ← decl.numNested rtr .inputs
+  return numLocalOutputs + numNestedInputs
+
+def GraphDecl.numDependencies (decl : GraphDecl) (rtr : Name) : ReactionDecl.DependencyKind → MacroM Nat
+  | .portSource => decl.numSources rtr
+  | .portEffect => decl.numEffects rtr
+  | .actionSource | .actionEffect => return (← decl.reactorWithName rtr).num .actions
 
 def InterfaceDecl.genInterfaceScheme (decl : InterfaceDecl) (name : Ident) : MacroM Command := 
   let schemeIdent := mkIdentFrom name (name.getId ++ `scheme)
@@ -90,6 +177,101 @@ def InterfaceDecl.genInterfaceScheme (decl : InterfaceDecl) (name : Ident) : Mac
       type var := match var with $[| $(decl.ids) => $types]*
   )
 
+def ReactionDecl.DependencyKind.allCases : Array DependencyKind := 
+  #[.portSource, .portEffect, .actionSource, .actionEffect]
+
+def ReactionDecl.DependencyKind.name : ReactionDecl.DependencyKind → Name
+  | .portSource   => `PortSource
+  | .portEffect   => `PortEffect
+  | .actionSource => `ActionSource
+  | .actionEffect => `ActionEffect
+
+def ReactionDecl.DependencyKind.injCoeTarget (graphIdent classIdent : Ident) : ReactionDecl.DependencyKind → MacroM Term 
+  | .portSource                  => `((Network.Graph.reactionInputScheme $graphIdent $classIdent |>.vars))
+  | .portEffect                  => `((Network.Graph.reactionOutputScheme $graphIdent $classIdent |>.vars))
+  | .actionSource |.actionEffect => `((Network.Graph.schemes $graphIdent $classIdent |>.interface .actions |>.vars))
+
+def ReactionDecl.genDependencyEnums (decl : ReactionDecl) (ns : Ident) : MacroM (Array Command) := do
+  ReactionDecl.DependencyKind.allCases.mapM fun kind =>
+    let enumIdent := mkIdentFrom ns (ns.getId ++ kind.name)
+    let ids := decl.dependencies kind
+    `(inductive $enumIdent $[| $ids:ident]* deriving DecidableEq)
+
+structure InjCoeGenDescription where
+  dependencyKind : ReactionDecl.DependencyKind
+  ids : Array Ident
+  graphIdent : Ident
+  noNsReactorIdent : Ident
+  reactionIdx : Nat
+  isComplete : Bool
+
+def InjCoeGenDescription.reactorIdent (descr : InjCoeGenDescription) :=
+  mkIdentFrom descr.noNsReactorIdent <| descr.graphIdent.getId ++ descr.noNsReactorIdent.getId
+
+def InjCoeGenDescription.classIdent (descr : InjCoeGenDescription) :=
+  mkIdentFrom descr.noNsReactorIdent <| descr.graphIdent.getId ++ `Class ++ descr.noNsReactorIdent.getId
+
+def InjCoeGenDescription.sourceTypeIdent (descr : InjCoeGenDescription) :=
+  mkIdentFrom descr.reactorIdent (descr.reactorIdent.getId ++ s!"Reaction{descr.reactionIdx}" ++ descr.dependencyKind.name)
+
+def InjCoeGenDescription.targetTypeTerm (descr : InjCoeGenDescription) :=
+  descr.dependencyKind.injCoeTarget descr.graphIdent descr.classIdent
+
+def InjCoeGenDescription.injCoeType (descr : InjCoeGenDescription) := do
+  `(InjectiveCoe $(descr.sourceTypeIdent) $(← descr.targetTypeTerm))
+
+def InjCoeGenDescription.sourceTerms (descr : InjCoeGenDescription) : MacroM (Array Term) := do
+  descr.ids.mapM fun id => `(.$id)
+
+def InjCoeGenDescription.sumTerms (descr : InjCoeGenDescription) : MacroM (Array Term) := 
+  descr.ids.mapM fun id => do
+    match id.getId with
+    | .str .anonymous l            => `(.inl .$(mkIdent <| .mkSimple l))
+    | .str (.str .anonymous rtr) l => `(.inr ⟨.$(mkIdent <| .mkSimple rtr), .$(mkIdent <| .mkSimple l)⟩)
+    | _                            => throwUnsupported 
+
+def InjCoeGenDescription.genInjectiveCoe (descr : InjCoeGenDescription) : MacroM Command := do
+  if descr.ids.isEmpty then 
+    forEmpty descr
+  else 
+    match descr.dependencyKind with 
+    | .portSource   | .portEffect => forPorts descr
+    | .actionSource | .actionEffect => forActions descr
+where
+  forEmpty (descr : InjCoeGenDescription) : MacroM Command := do 
+    `(
+      @[reducible] instance : $(← descr.injCoeType) where
+        coe := (·.casesOn)
+        inv _ := none
+    )
+  forPorts (descr : InjCoeGenDescription) : MacroM Command := do
+    let sumTerms ← descr.sumTerms
+    let mut invSrcTerms := sumTerms
+    let mut invDstTerms ← (← descr.sourceTerms).mapM fun term => `(some $term)
+    unless descr.isComplete do
+      invSrcTerms := invSrcTerms.push (← `(_))
+      invDstTerms := invDstTerms.push (← `(Option.none))
+    `(
+      @[reducible] instance : $(← descr.injCoeType) where
+        coe $[| $(← descr.sourceTerms) => $sumTerms]*
+        inv $[| $invSrcTerms => $invDstTerms]*
+    )
+  forActions (descr : InjCoeGenDescription) : MacroM Command := do
+    let coeDstIdents ← descr.sourceTerms
+    let mut invSrcTerms : Array Term ← descr.sourceTerms
+    let mut invDstTerms : Array Term ← (← descr.sourceTerms).mapM fun ident => `(some $ident)
+    unless descr.isComplete do 
+      invSrcTerms := invSrcTerms.push (← `(_))
+      invDstTerms := invDstTerms.push (← `(Option.none))
+    `(
+      @[reducible] instance : $(← descr.injCoeType) where
+        coe $[| $(← descr.sourceTerms) => $coeDstIdents]*
+        inv $[| $invSrcTerms => $invDstTerms]*
+    )
+
+def Reactor.InterfaceKind.allCases : Array Reactor.InterfaceKind :=
+  #[.inputs, .outputs, .actions, .state]
+
 def Reactor.InterfaceKind.name : Reactor.InterfaceKind → Name
   | .inputs  => `Input
   | .outputs => `Output
@@ -97,20 +279,24 @@ def Reactor.InterfaceKind.name : Reactor.InterfaceKind → Name
   | .state   => `State
 
 def ReactorDecl.genInterfaceSchemes (decl : ReactorDecl) (ns : Ident) : MacroM (Array Command) :=
-  #[Reactor.InterfaceKind.inputs, .outputs, .actions, .state].mapM fun kind =>
+  Reactor.InterfaceKind.allCases.mapM fun kind =>
     let interface := decl.interfaces kind
     interface.genInterfaceScheme <| mkIdentFrom decl.name (ns.getId ++ decl.name.getId ++ kind.name)
+
+def ReactorDecl.genReactionDependencyEnums (decl : ReactorDecl) (ns : Ident) : MacroM (Array Command) := do
+  decl.reactions.enumerate.concatMapM fun ⟨idx, rcn⟩ => do
+    let rcnNs := mkIdentFrom decl.name (ns.getId ++ decl.name.getId ++ s!"Reaction{idx}")
+    rcn.genDependencyEnums rcnNs
 
 def ReactorDecl.genReactorScheme (decl : ReactorDecl) (ns : Ident) : MacroM Command := do
   let mkNamespacedIdent name := mkIdentFrom decl.name (ns.getId ++ decl.name.getId ++ name)
   let classesEnumIdent := mkIdentFrom ns (ns.getId ++ `Class)
   let nestedEnumIdent := mkNamespacedIdent `Nested
-  let schemeIdent := mkNamespacedIdent `scheme
   let namespacedNestedIds := decl.nested.ids.map fun id => mkIdentFrom id (nestedEnumIdent.getId ++ id.getId)
   let classes := decl.nested.values.map fun «class» => mkIdentFrom «class» (classesEnumIdent.getId ++ «class».getId)
   `(
     inductive $nestedEnumIdent $[| $(decl.nested.ids):ident]* deriving DecidableEq
-    abbrev $schemeIdent : Reactor.Scheme $classesEnumIdent where
+    abbrev $(mkNamespacedIdent `scheme) : Reactor.Scheme $classesEnumIdent where
       interface -- TODO: Use `Reactor.InterfaceKind.name` for this.
         | .inputs  => $(mkIdent `Input.scheme)
         | .outputs => $(mkIdent `Output.scheme)
@@ -118,8 +304,8 @@ def ReactorDecl.genReactorScheme (decl : ReactorDecl) (ns : Ident) : MacroM Comm
         | .state   => $(mkIdent `State.scheme)
       children := $nestedEnumIdent
       «class» child := match child with $[| $namespacedNestedIds => $classes]*
-  )
-  
+  )  
+
 def GraphDecl.genClassesEnum (decl : GraphDecl) : MacroM Command := do
   let enumIdent := mkIdentFrom decl.name (decl.name.getId ++ `Class)
   `(inductive $enumIdent $[| $(decl.reactorNames):ident]*)
@@ -135,11 +321,28 @@ def GraphDecl.genGraphInstance (decl : GraphDecl) : MacroM Command := do
     schemes $[| $classes => $classSchemes]*
   )
 
+def GraphDecl.genInjectiveCoes (decl : GraphDecl) : MacroM (Array Command) :=
+  decl.reactors.concatMapM fun rtr =>
+    rtr.reactions.enumerate.concatMapM fun ⟨idx, rcn⟩ =>
+      ReactionDecl.DependencyKind.allCases.mapM fun kind => do
+        let ids := rcn.dependencies kind
+        InjCoeGenDescription.genInjectiveCoe {
+          dependencyKind := kind
+          ids := ids
+          graphIdent := decl.name
+          noNsReactorIdent := rtr.name
+          reactionIdx := idx
+          isComplete := ids.size = (← decl.numDependencies rtr.name.getId kind)
+        }
+
 def GraphDecl.genInterfaceSchemes (decl : GraphDecl) : MacroM (Array Command) :=
-  decl.reactors.concatMapM fun reactor => reactor.genInterfaceSchemes decl.name
+  decl.reactors.concatMapM (·.genInterfaceSchemes decl.name)
 
 def GraphDecl.genReactorSchemes (decl : GraphDecl) : MacroM (Array Command) :=
   decl.reactors.mapM (·.genReactorScheme decl.name)
+
+def GraphDecl.genReactionDependencyEnums (decl : GraphDecl) : MacroM (Array Command) := do
+  decl.reactors.concatMapM (·.genReactionDependencyEnums decl.name)
 
 macro "gen_graph" graph:graph_decl : command => do
   let graph ← GraphDecl.fromSyntax graph
@@ -147,4 +350,6 @@ macro "gen_graph" graph:graph_decl : command => do
     (← graph.genInterfaceSchemes) ++ 
     [← graph.genClassesEnum] ++
     (← graph.genReactorSchemes) ++
-    [← graph.genGraphInstance]
+    [← graph.genGraphInstance] ++
+    (← graph.genReactionDependencyEnums) ++
+    (← graph.genInjectiveCoes)

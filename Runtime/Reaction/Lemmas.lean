4c2ec765678a72d65e0c7f9d82ca23bf22638f5e
Marcus Rossel <marcusrossel@icloud.com>
2022-11-23 20:29:24 +0100

    Simplify ReactionT lemma gen

--- Runtime/Reaction/Lemmas.lean
+++ Runtime/Reaction/Lemmas.lean
@@ -3,51 +3,46 @@ import Runtime.Reaction.Monad
 namespace ReactionT
 
 open Lean in
-scoped macro "mk_rfl_lemmas " op:ident field:ident : command => `(
-  @[simp] theorem $(mkIdentFrom op s!"{op.getId}_value")  {σPS σPE σAS σAE σS σP input var} : ($op (σPS := σPS) (σPE := σPE) (σAS := σAS) (σAE := σAE) (σS := σS) (σP := σP) (m := Id) var input).snd = input.$field var  := rfl
-  @[simp] theorem $(mkIdentFrom op s!"{op.getId}_state")  {σPS σPE σAS σAE σS σP input var} : ($op (σPS := σPS) (σPE := σPE) (σAS := σAS) (σAE := σAE) (σS := σS) (σP := σP) (m := Id) var input).fst.state = input.state := rfl
-  @[simp] theorem $(mkIdentFrom op s!"{op.getId}_ports")  {σPS σPE σAS σAE σS σP input var} : ($op (σPS := σPS) (σPE := σPE) (σAS := σAS) (σAE := σAE) (σS := σS) (σP := σP) (m := Id) var input).fst.ports.isEmpty       := rfl
-  @[simp] theorem $(mkIdentFrom op s!"{op.getId}_events") {σPS σPE σAS σAE σS σP input var} : ($op (σPS := σPS) (σPE := σPE) (σAS := σAS) (σAE := σAE) (σS := σS) (σP := σP) (m := Id) var input).fst.events.isEmpty      := rfl
-)
+scoped macro "mk_get_lemmas" op:ident field:ident param:"_"? : command => do
+  let var := if param.isSome then #[mkIdent `var] else #[]
+  let input := mkIdent `input
+  let opApp ← `($op $[ $var ]* $input (m := Id)
+    (σPS := $(mkIdent `σPS)) (σPE := $(mkIdent `σPE))
+    (σAS := $(mkIdent `σAS)) (σAE := $(mkIdent `σAE))
+    (σS  := $(mkIdent  `σS)) (σP  := $(mkIdent  `σP))
+  )
+  let lemmas := #[
+    ("value",        ← `(($opApp).snd = $input.$field $[ $var ]*)),
+    ("state",        ← `(($opApp).fst.state = $(input).state)),
+    ("ports",        ← `(($opApp).fst.ports.isEmpty)),
+    ("events",       ← `(($opApp).fst.events.isEmpty)),
+    ("writtenPorts", ← `(($opApp).fst.writtenPorts.isEmpty))
+  ]
+  let commands ← lemmas.mapM fun ⟨suffix, property⟩ => `(
+    @[simp] theorem $(mkIdentFrom op s!"{op.getId}_{suffix}") {$[ $var ]*} : $property := rfl
+  )
+  return ⟨mkNullNode commands⟩
 
-open Lean in
-scoped macro "mk_rfl_lemmas' " op:ident field:ident : command => `(
-  @[simp] theorem $(mkIdentFrom op s!"{op.getId}_value")  {σPS σPE σAS σAE σS σP input} : ($op (σPS := σPS) (σPE := σPE) (σAS := σAS) (σAE := σAE) (σS := σS) (σP := σP) (m := Id) input).snd = input.$field      := rfl
-  @[simp] theorem $(mkIdentFrom op s!"{op.getId}_state")  {σPS σPE σAS σAE σS σP input} : ($op (σPS := σPS) (σPE := σPE) (σAS := σAS) (σAE := σAE) (σS := σS) (σP := σP) (m := Id) input).fst.state = input.state := rfl
-  @[simp] theorem $(mkIdentFrom op s!"{op.getId}_ports")  {σPS σPE σAS σAE σS σP input} : ($op (σPS := σPS) (σPE := σPE) (σAS := σAS) (σAE := σAE) (σS := σS) (σP := σP) (m := Id) input).fst.ports.isEmpty       := rfl
-  @[simp] theorem $(mkIdentFrom op s!"{op.getId}_events") {σPS σPE σAS σAE σS σP input} : ($op (σPS := σPS) (σPE := σPE) (σAS := σAS) (σAE := σAE) (σS := σS) (σP := σP) (m := Id) input).fst.events.isEmpty      := rfl
-)
+mk_get_lemmas getInput       ports   _
+mk_get_lemmas getState       state   _
+mk_get_lemmas getAction      actions _
+mk_get_lemmas getParam       params  _
+mk_get_lemmas getTag         tag
+mk_get_lemmas getLogicalTime time
 
-mk_rfl_lemmas  getInput       ports  
-mk_rfl_lemmas  getState       state  
-mk_rfl_lemmas  getAction      actions
-mk_rfl_lemmas  getParam       params 
-mk_rfl_lemmas' getTag         tag
-mk_rfl_lemmas' getLogicalTime time
+-- TODO: Lemmas for `setOutput`, `setState`, `schedule` and `requestStop`.
 
-/-
-theorem setOutput_def : input -[setOutput port v]→ (·.fst.ports port = v) := by
-  exists do
-    let ports := fun p => if h : p = port then some (h ▸ v) else none
-    return ⟨({ ports, state := input.state }, ()), by simp⟩
+@[simp] theorem setOutput_same_port : (setOutput (m := Id) (σPS := σPS) (σPE := σPE) (σAS := σAS) (σAE := σAE) (σS := σS) (σP := σP) var val input).fst.ports var = val := by
+  simp [setOutput]
 
-theorem setState_eq_new_val : input -[setState stv v]→ (·.fst.state stv = v) := by
-  exists do
-    let state := fun s => if h : s = stv then h ▸ v else input.state s
-    return ⟨({ state }, ()), by simp⟩
+@[simp] theorem setOutput_other_port {var var' val} : (var' ≠ var) → (setOutput (σPS := σPS) (σPE := σPE) (σAS := σAS) (σAE := σAE) (σS := σS) (σP := σP) (m := Id) var val input).fst.ports var' = none :=
+  (by simp [setOutput, ·])
 
-theorem setState_eq_old_val : input -[setState stv v]→ (fun out => ∀ stv', (stv' ≠ stv) → out.fst.state stv' = input.state stv') := by
-  exists do
-    let state := fun s => if h : s = stv then h ▸ v else input.state s
-    return ⟨({ state }, ()), by simp_all⟩
+@[simp] theorem setState_same_state : (setState (σPS := σPS) (σPE := σPE) (σAS := σAS) (σAE := σAE) (σS := σS) (σP := σP) (m := Id) stv v input).fst.state stv = v := by
+  simp [setState]
 
-theorem schedule_def : input -[schedule action delay v]→ (·.fst.events = #[⟨action, v, input.time.advance delay⟩]#) := by
-  exists do
-    let time := input.time.advance delay
-    return ⟨({ state := input.state, events := #[{ action, time, value := v }]# }, ()), by simp⟩
-
-theorem schedule_state : input -[schedule action delay v]→ (·.fst.state var = input.state var) := sorry
-
--/
+@[simp] theorem setState_other_state {stv stv' v} : (stv' ≠ stv) → (setState (σPS := σPS) (σPE := σPE) (σAS := σAS) (σAE := σAE) (σS := σS) (σP := σP) (m := Id) stv v input).fst.state stv' = input.state stv' := by
+  intro h
+  simp [setState, h]
 
 end ReactionT
